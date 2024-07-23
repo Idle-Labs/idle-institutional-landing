953a9a25f82792d2962fd19a08eb363087ca18c9
Nicolò <nicandman@gmail.com>
2024-07-23 17:12:51 +0200

    Updates

---
+++ server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/nikiman/Projects/idle-institutional-landing/src/app/layout":["static/media/3d9ea938b6afa941-s.p.woff2","static/media/ca13452f97433645-s.p.woff2","static/media/a9b61b60c2d733b4-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
---
+++ server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"y87nYKAerpb5rAoy12ZTypj+Cw1vpj+nL+PEuo9iEPA=\"}"
