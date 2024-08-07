61dbff75eaa2af322791b90b1cec27d240ca9720
Nicolò <nicandman@gmail.com>
2024-08-07 12:58:43 +0200

    Update OG data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,32 @@
-import type { Metadata } from "next";
-import "./globals.scss"
+import type { Metadata } from 'next'
+import './globals.scss'
 import { Providers } from './providers'
 
+const baseUrl = new URL('https://institutional.idle.finance/')
 export const metadata: Metadata = {
-  title: "Idle Institutional",
-  description: "Idle Istitutional early access",
-};
+  metadataBase: baseUrl,
+  title: 'Idle Institutional',
+  description:
+    'Modernizing credit markets to bring speed, programmability, and compliance advantages of blockchain tokenization.',
+  openGraph: {
+    type: 'website',
+    url: baseUrl,
+    title: 'Idle Institutional',
+    description:
+      'Modernizing credit markets to bring speed, programmability, and compliance advantages of blockchain tokenization.',
+    siteName: 'Idle Institutional',
+    images: [
+      {
+        url: '/logos/idle-og.png',
+      },
+    ],
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
@@ -18,5 +34,5 @@ export default function RootLayout({
         <Providers>{children}</Providers>
       </body>
     </html>
-  );
+  )
 }
